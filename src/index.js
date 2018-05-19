d36d1750c615a39f51a4861877ff9e55fc09af20
crusoexia <xiawenlan1984@gmail.com>
2018-05-19 12:45:23 +0800

    Render char

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const { compose, first } = require('lodash/fp');
-const { of, Observable, fromEvent, merge } = require('rxjs');
-const { filter, mapTo, buffer, map } = require('rxjs/operators');
-const { renderKeyStroke } = require('./render');
+const { of, Observable, fromEvent, merge, timer } = require('rxjs');
+const { filter, mapTo, buffer, map, debounce, startWith, first: firstRx, concat } = require('rxjs/operators');
+const { renderCode, renderChar } = require('./render');
 
 const DASH_DETEMINE_HOLD_MS_THRESHOLD = 200;
+const WORD_DETEMINE_HALT_MS_THRESHOLD = 1000;
 
 function streamFromEvent(eventName) {
   return Observable.create(observer => {
@@ -28,13 +29,19 @@ render();
 window.addEventListener('load', () => {
   const enterKeyDownStream = streamFromEvent('keydown').pipe(filter(isEnterKey));
   const enterKeyUpStream = fromEvent(document, 'keyup').pipe(filter(isEnterKey));
+  const codeStream = enterKeyDownStream.pipe(
+    map(() => Date.now()),
+    buffer(enterKeyUpStream), 
+    map(first),
+    map(firstDownTime => Date.now() - firstDownTime > DASH_DETEMINE_HOLD_MS_THRESHOLD ? 'DASH' : 'DOT')
+  );
+  const charStream = codeStream.pipe(
+    buffer(codeStream.pipe(debounce(() => timer(WORD_DETEMINE_HALT_MS_THRESHOLD)))),
+  );
 
-  enterKeyDownStream
-    .pipe(
-      map(() => Date.now()),
-      buffer(enterKeyUpStream), 
-      map(first),
-      map(firstDownTime => Date.now() - firstDownTime > DASH_DETEMINE_HOLD_MS_THRESHOLD ? 'DASH' : 'DOT')
-    )
-    .subscribe(code => renderKeyStroke(document.getElementById('code_history'), code));
-})
+  codeStream.pipe(
+    firstRx(),
+    concat(charStream)
+  ).subscribe(() => renderChar(document.getElementById('code_logs')));
+  codeStream.subscribe(code => renderCode(document.getElementById('code_logs').lastElementChild, code));
+});
--- src/render.js
+++ src/render.js
@@ -1,9 +1,18 @@
-function renderKeyStroke(container, code) {
+const { curry } = require('lodash/fp');
+
+const renderCode = curry(function (container, code) {
   const el = document.createElement('div');
   el.className = `code ${code.toLowerCase()}`;
+  container.appendChild(el);
+});
+
+function renderChar(container) {
+  const el = document.createElement('div');
+  el.className = 'char';
   container.appendChild(el);
 }
 
 module.exports = {
-  renderKeyStroke
+  renderCode,
+  renderChar
 };
